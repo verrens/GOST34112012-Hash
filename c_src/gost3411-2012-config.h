b328ce0b44ea6b7373e2d20f38418af91cb35ef6
Denis Afonin <verrens@yandex.ru>
2024-04-03 14:24:27 +0300

    Version 0.1.1.0

---
+++ c_src/gost3411-2012-config.h
@@ -0,0 +1,12 @@
+#ifndef __GOST3411_LITTLE_ENDIAN__
+#define __GOST3411_LITTLE_ENDIAN__
+#endif
+#ifndef __GOST3411_HAS_MMX__
+#define __GOST3411_HAS_MMX__
+#endif
+#ifndef __GOST3411_HAS_SSE2__
+#define __GOST3411_HAS_SSE2__
+#endif
+#ifndef __GOST3411_HAS_SSE41__
+#define __GOST3411_HAS_SSE41__
+#endif
